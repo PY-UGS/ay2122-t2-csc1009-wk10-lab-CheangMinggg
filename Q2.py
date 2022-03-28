4b9903a908723ccd2da0de373a39e41286c01206
CheangMinggg <98368762+CheangMinggg@users.noreply.github.com>
2022-03-28 15:27:16 +0800

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,5 @@
+from tokenize import Double
+
+x = float(input("\nEnter x value: ")) #input 
+y = float(input("\nEnter y value :"))
+print("Average of",x, "and", y, "=", (x + y)/2 ) #average
---
+++ Q3a.py
@@ -0,0 +1,9 @@
+Mod  = input("Enter a module code: ")
+switch = { #switch case
+    "CSC1006": "Mathematics 2",
+    "CSC1007": "Operating Systems",
+    "CSC1008": "Data Structures and Algorithms",
+    "CSC1009": "OOP",
+    "CSC1010": "Computer Networks"
+}
+print(switch[Mod])
